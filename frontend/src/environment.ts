094fdd130d2b8bfa9d9eb8d6bc3316bb59d1d071
unlvy <unlv13y@gmail.com>
2022-12-21 11:47:33 +0100

    removed additional slash from backed url

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,4 +1,4 @@
-const backendUrl = 'https://album-recommender-backend.onrender.com/';
+const backendUrl = 'https://album-recommender-backend.onrender.com';
 
 export const environment = {
   loginUrl: `${backendUrl}/auth/login`,
